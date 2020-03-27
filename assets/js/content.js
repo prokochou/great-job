4342f76b68003220439a87939c1f82a62bbd1cc4
proko.chou <proko.chou@cloudmosa.com>
2020-03-27 09:14:42 +0800

    Modify the content

---
+++ assets/js/content.js
@@ -0,0 +1,128 @@
+// open and close vocabulary by clicking sentence
+$(document).ready(function(){
+
+  $("#sentence-1").click(function(){
+	if($("#word-1").css("display")=="none"){
+		$("#word-1").show();
+		$("#sentence-1 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-1").hide();
+		$("#sentence-1 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+
+  $("#sentence-2").click(function(){
+	if($("#word-2").css("display")=="none"){
+		$("#word-2").show();
+		$("#sentence-2 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-2").hide();
+		$("#sentence-2 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+  $("#sentence-3").click(function(){
+	if($("#word-3").css("display")=="none"){
+		$("#word-3").show();
+		$("#sentence-3 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-3").hide();
+		$("#sentence-3 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+	
+  $("#sentence-4").click(function(){
+	if($("#word-4").css("display")=="none"){
+		$("#word-4").show();
+		$("#sentence-4 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-4").hide();
+		$("#sentence-4 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+  $("#sentence-5").click(function(){
+	if($("#word-5").css("display")=="none"){
+		$("#word-5").show();
+		$("#sentence-5 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-5").hide();
+		$("#sentence-5 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+
+  $("#sentence-6").click(function(){
+	if($("#word-6").css("display")=="none"){
+		$("#word-6").show();
+		$("#sentence-6 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-6").hide();
+		$("#sentence-6 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+  $("#sentence-7").click(function(){
+	if($("#word-7").css("display")=="none"){
+		$("#word-7").show();
+		$("#sentence-7 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-7").hide();
+		$("#sentence-7 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+	
+  $("#sentence-8").click(function(){
+	if($("#word-8").css("display")=="none"){
+		$("#word-8").show();
+		$("#sentence-8 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-8").hide();
+		$("#sentence-8 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+  $("#sentence-9").click(function(){
+	if($("#word-9").css("display")=="none"){
+		$("#word-9").show();
+		$("#sentence-9 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-9").hide();
+		$("#sentence-9 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+
+  $("#sentence-10").click(function(){
+	if($("#word-10").css("display")=="none"){
+		$("#word-10").show();
+		$("#sentence-10 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-10").hide();
+		$("#sentence-10 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+  $("#sentence-11").click(function(){
+	if($("#word-11").css("display")=="none"){
+		$("#word-11").show();
+		$("#sentence-11 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-11").hide();
+		$("#sentence-11 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+
+  $("#sentence-12").click(function(){
+	if($("#word-12").css("display")=="none"){
+		$("#word-12").show();
+		$("#sentence-12 .chinese-sentence").css("color","#0062cc");
+	}else{
+		$("#word-12").hide();
+		$("#sentence-12 .chinese-sentence").css("color","#000000b5");
+		}
+	});
+});
