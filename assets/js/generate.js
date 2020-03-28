61e55a69535fc1f2385e514ecd65123cec5003b3
proko.chou <proko.chou@cloudmosa.com>
2020-03-28 19:34:50 +0800

    Add template.

--- assets/js/generate.js
+++ assets/js/generate.js
@@ -1,14 +1,41 @@
-// Add one more paragraph
-$(document).ready(function(){
-  $("#add-content").click(function(){
-  	console.log('test');
-  	$("#more-paragraph").append('<div class="form-group"><label for="chinese-text"><b>Chinese Text</b></label><textarea class="form-control" rows="3"></textarea required></div><div class="form-group"><label for="chinese-text"><b>English Text</b></label><textarea class="form-control" rows="3"></textarea required></div>'
-  	);
 
-  });
+$(document).ready(function(){
+	// Add one more paragraph
+	$("#add-content").click(function(){
+	  	$("#more-paragraph").append('<div class="form-group"><label for="chinese-text"><b>Chinese Text</b></label><textarea class="form-control" rows="3"></textarea required></div><div class="form-group"><label for="chinese-text"><b>English Text</b></label><textarea class="form-control" rows="3"></textarea required></div>'
+	  	);
+  	
+  	});
+
+	// Add Practice or Discussion
+
+	$("#add-part2").click(function(){
+	  	$("#more-paragraph").append('<div class="form-group"><label for="chinese-text"><b>Practice or Discussion</b></label><textarea class="form-control" rows="3"></textarea required></div>'
+	  	);
+  	
+  	});
+
+	// get json file
+
+	$(function() {
+		var people = [];
+
+   		$.getJSON('template.json', function(data) {
+
+	       $.each(data.person, function(i, f) {
+	          var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" +
+	           "<td>" + f.lastName + "</td>" + "<td>" + f.job + "</td>" + "<td>" + f.roll + "</td>" + "</tr>"
+	           $(tblRow).appendTo("#userdata tbody");
+	     });
+
+   	});
+
 });
 
-// plan-new.html
+
+});
+
+
 function create(){
 	var url = "file:///Users/prokochou/Documents/great-job/content-template.html";
 	window.location.assign(url);
